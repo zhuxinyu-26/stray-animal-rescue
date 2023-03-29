de60f8f7dce1c0ddb36f728b0b0bdd4dfe8af274
Xinyu Zhu <zhuxinyu_26@163.com>
2023-03-29 02:10:04 -0400

    finish setup

--- controllers/animals.js
+++ controllers/animals.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
 
 /* GET animals listing. */
 router.get('/', (req, res) => {
-  res.render('animals/index', { title: 'animal list' });
+  fs.readFile('./data/animals.json', 'utf8', (err, animals) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(animals);
+      res.render('animals/index', {
+        title: 'Animal List',
+        animals: JSON.parse(animals),
+      });
+    }
+  });
 });
-
 module.exports = router;

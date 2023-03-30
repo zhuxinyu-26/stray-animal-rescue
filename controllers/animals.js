64b0c0e5b2f5adec7f036f7b8f68c7328d4b9db6
Xinyu Zhu <zhuxinyu_26@163.com>
2023-03-29 21:17:23 -0400

    animals connected to mgdb

--- controllers/animals.js
+++ controllers/animals.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
+// const fs = require('fs');
 const Animal = require('../models/animals');
 
 /* GET animals listing. */
 router.get('/', (req, res) => {
   //get data from json file
-  fs.readFile('./data/animals.json', 'utf8', (err, animals) => {
+  //   fs.readFile('./data/animals.json', 'utf8', (err, animals) => {
+  //     if (err) {
+  //       console.log(err);
+  //     } else {
+  //       console.log(animals);
+  //       res.render('animals/index', {
+  //         title: 'Animal List',
+  //         animals: JSON.parse(animals),
+  //       });
+  //     }
+  //   });
+  //get data from mgdb using animals model
+  Animal.find((err, animals) => {
     if (err) {
       console.log(err);
     } else {
       console.log(animals);
       res.render('animals/index', {
         title: 'Animal List',
-        animals: JSON.parse(animals),
+        animals: animals,
       });
     }
   });

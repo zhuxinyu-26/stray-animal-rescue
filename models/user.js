ac7da92e6cdf558a571f0c72b53fb7018e089fbb
Xinyu Zhu <zhuxinyu_26@163.com>
2023-03-30 16:50:07 -0400

    added google authentication in .env and app.js

--- models/user.js
+++ models/user.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose');
 // extend the functionality of this model as it's used for authentication
 const plm = require('passport-local-mongoose');
-// const findOrCreate = require('mongoose-findorcreate');
+const findOrCreate = require('mongoose-findorcreate');
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  //   oauthProvider: String,
-  //   oauthId: String,
+  oauthProvider: String,
+  oauthId: String,
 });
 
 // convert this model from a regular model to one that inherits all the abilities of user management
 userSchema.plugin(plm);
-// userSchema.plugin(findOrCreate);
+userSchema.plugin(findOrCreate);
 
 module.exports = mongoose.model('User', userSchema);
